3e26e1f5d9d5c344b96635631a0a75bd2c8b365a
Nicole-Benavides <112515965+Nicole-Benavides@users.noreply.github.com>
2023-07-24 22:56:33 -0500

    Primer Commit - Pagina Completa

---
+++ js/Main.js
@@ -0,0 +1,22 @@
+function Suma(){
+
+    // Variables y su tipo
+    let A = 0;
+    let B = 0;
+    let SUMA = 0;
+
+    // Capturar variable 
+
+    // prompt = Solo recibe string
+    // parseInt = Convertir string en INT
+    A = parseInt(prompt("Por favor ingrese el primer valor"));
+    B = parseInt(prompt("Por favor ingrese el segundo valor"));
+    
+
+    SUMA = A + B;
+
+    alert("La suma es igual a:  " +  SUMA);
+
+
+}
+
